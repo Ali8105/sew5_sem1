af874e5cb67426e3375339358b20210e4a7e0956
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-02 21:25:33 +0200

    man muss doch LED und Taster programmierne

--- src/Test_UML.java
+++ src/Test_UML.java
@@ -1,20 +1,29 @@
 public class Test_UML {
 
     public static void main(String[] args) {
-        Taster taster1 = new Taster();
-        Taster taster2 = new Taster();
-        FlipFlop ff = new FlipFlop("FF");
+        Taster taster1 = new Taster("taster1");
+        Taster taster2 = new Taster("taster2");
+        FlipFlop FF = new FlipFlop("FF");
         LED led = new LED();
+
+        FF.connect(0, taster1, 0);
         
+
     }
 }
 
 
-class Taster{
-    Taster(){
+class Taster extends Components{
+    Taster(String name){
+        super(name, 1);
         /**
          * Brauchen wir nicht nur damit keine Fehler auftretten
          */
+    }
+
+    @Override
+    void calc() {
+
     }
 }
 
