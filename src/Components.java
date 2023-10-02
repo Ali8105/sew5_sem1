a8d5f6c97ca6233027342f292dcc420e890e222e
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-02 20:59:07 +0200

    FlipFlop komplett falsch

--- src/Components.java
+++ src/Components.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 abstract  class Components {
@@ -16,7 +17,16 @@ abstract  class Components {
      * List von Input Pins
      */
     List<Input> pinsIn;
-    
+
+    public Components(String name, int outputs){
+        this.name = name;
+        pinsOut = new ArrayList<>();
+        pinsIn = new ArrayList<>();
+        for (int i = 0; i < outputs; i++) {
+            pinsOut.add(i,false);
+        }
+    }
+
     abstract void calc();
 
 }
