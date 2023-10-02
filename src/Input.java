c11f42c3837a3d4b1cdb0d3a44ad0ccdd40ca840
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-02 20:49:32 +0200

    Input

---
+++ src/Input.java
@@ -0,0 +1,17 @@
+public class Input {
+
+    /**
+     * Komponenten
+     */
+    Components component;
+
+    /**
+     * Source Port
+     */
+    int srcPort;
+
+    /**
+     * State
+     */
+    boolean state;
+}
