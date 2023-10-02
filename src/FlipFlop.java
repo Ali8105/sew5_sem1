3f0cfbc49973168a6e111293aaff3248bb9f1e5a
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-02 14:59:33 +0200

    JavaDoc

--- src/FlipFlop.java
+++ src/FlipFlop.java
@@ -3,28 +3,53 @@
  */
 public class FlipFlop {
 
+    /**
+     * Variable vom State/Status
+     */
     boolean state = false;
 
+    /**
+     * Set Interface
+     */
     public void Set(){
         state = true;
     }
 
+    /**
+     * Reset Interface
+     */
     public void Reset(){
         state = false;
     }
 
+    /**
+     * Q Interface
+     * @return liefert den Status vom Q Interface
+     */
     public boolean Q(){
         return state;
     }
 
+    /**
+     * !Q Interface
+     * @return liefert den nicht Status
+     */
     public boolean notQ(){
         return !state;
     }
 
+    /**
+     * Damit statt Object die state geliefert wird
+     * @return state
+     */
     public String toString(){
         return Boolean.toString(state);
     }
 
+    /**
+     * Testen der FlipFlop Klasse
+     * @param args
+     */
     public static void main(String[] args) {
         FlipFlop f = new FlipFlop();
         f.Set();
