018c7882d571f33db9662f4c824ef19d78df19e0
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-05 20:46:59 +0200

    JavaDoc

--- src/Taster.java
+++ src/Taster.java
@@ -1,19 +1,30 @@
+/**
+ * @author aligr
+ * Klasse Taster
+ */
 class Taster extends Components{
+
     Taster(String name){
         super(name, 1);
-        /**
-         * Brauchen wir nicht nur damit keine Fehler auftretten
-         */
     }
 
+    /**
+     * Wenn der Taster gedrückt wird, wird der Output auf true gesetzt
+     */
     public void press(){
         this.pinsOut.set(0,true);
     }
 
+    /**
+     * Wenn der Taster "losgelassen" wird, wird der Output wieder auf false gesetzt
+     */
     public void release(){
         this.pinsOut.set(0,false);
     }
 
+    /**
+     * leer calculate Methode wird nicht verwendet --> uberschreiben da geerbt
+     */
     @Override
     void calc() {
 
