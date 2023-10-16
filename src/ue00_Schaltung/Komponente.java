8e1552ca4452f11a9b36b5f2d7d2f3f05a74d149
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-16 14:58:54 +0200

    JavaDoc

--- src/ue00_Schaltung/Komponente.java
+++ src/ue00_Schaltung/Komponente.java
@@ -4,16 +4,34 @@ import java.util.List;
 
 abstract  class Komponente {
 
+    /**
+     * Name vom Komponente
+     */
     String name;
+    /**
+     * Liste von inputs
+     */
     List<Schnittstelle> inputs;
+    /**
+     * Lsite von outputs
+     */
     List<Schnittstelle> outputs;
 
+    /**
+     * Konstruktor Komponente
+     * @param name Name
+     * @param inputs Inputs
+     * @param outputs Outputs
+     */
     Komponente(String name, List<Schnittstelle> inputs, List<Schnittstelle> outputs){
         this.name = name;
         this.inputs = inputs;
         this.outputs = outputs;
     }
 
+    /**
+     * Abstracte Methode calc
+     */
     abstract void calc();
 
 }
